894edbec3c9bef98c20ce3ff86c4e9eb8d885287
Lucas Luan Pontarolo <lucaspontarolo@gmail.com>
2020-03-08 14:15:07 -0300

    Model for User and database configuration

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
-const { Router } = require('express')
+import Router from 'express'
+
+import User from './app/models/User'
 
 const routes = new Router()
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Teste' }))
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Lucas Luan',
+    email: 'lucas@email.com',
+    password_hash: '123456789',
+  })
+
+  return res.json(user)
+})
 
 export default routes
