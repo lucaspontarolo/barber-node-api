d68d9eff0a23305adbf0c666e9d5b225b5155d5d
Lucas Luan Pontarolo <lucaspontarolo@gmail.com>
2020-03-09 22:43:20 -0300

    List of providers and configs to provide uploads

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import multer from 'multer'
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
 import FileController from './app/controllers/FileController'
+import ProviderController from './app/controllers/ProviderController'
 
 import authMiddleware from './app/middlewares/auth'
 import multerConfig from './config/multer'
@@ -17,5 +18,7 @@ routes.post('/users', UserController.store)
 routes.use(authMiddleware)
 routes.put('/users', authMiddleware, UserController.update)
 routes.post('/files', upload.single('file'), FileController.store)
+
+routes.get('/providers', ProviderController.index)
 
 export default routes
