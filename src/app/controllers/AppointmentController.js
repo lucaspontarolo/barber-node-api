c75a6b52393581ef946090f8c288fed1cece7b19
Lucas Luan Pontarolo <lucaspontarolo@gmail.com>
2020-03-10 21:36:06 -0300

    List appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,37 @@
 import * as yup from 'yup'
+import { startOfHour, isBefore, parseISO } from 'date-fns'
 import Appointment from '../models/Appointment'
 import User from '../models/User'
+import File from '../models/File'
 
 class AppointmentController {
+  async index(req, res) {
+    const appointments = await Appointment.findAll({
+      attributes: ['id', 'date'],
+      where: {
+        user_id: req.userId,
+        canceled_at: null,
+      },
+      order: ['date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    })
+
+    return res.json(appointments)
+  }
+
   async store(req, res) {
     const { provider_id, date } = req.body
 
@@ -19,6 +48,25 @@ class AppointmentController {
     const isProvider = await User.findOne({
       where: { id: provider_id, provider: true },
     })
+
+    const hourStart = startOfHour(parseISO(date))
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past date are not permited.' })
+    }
+
+    const checkAvaliability = await Appointment.findOne({
+      where: {
+        provider_id,
+        cancelled_at: null,
+        date: hourStart,
+      },
+    })
+
+    if (checkAvaliability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointent date is not avaliable.' })
+    }
 
     if (!isProvider) {
       return res
