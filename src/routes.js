a5ebcb18e5d2582a135ac5590cce0527a906b0ed
Lucas Luan Pontarolo <lucaspontarolo@gmail.com>
2020-03-16 23:35:07 -0300

    Added avaliable appointments of day

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import FileController from './app/controllers/FileController'
 import ProviderController from './app/controllers/ProviderController'
 import AppointmentController from './app/controllers/AppointmentController'
 import NotificationController from './app/controllers/NotificationController'
+import AvaliableController from './app/controllers/AvaliableController'
 
 import authMiddleware from './app/middlewares/auth'
 import multerConfig from './config/multer'
@@ -22,6 +23,7 @@ routes.put('/users', authMiddleware, UserController.update)
 routes.post('/files', upload.single('file'), FileController.store)
 
 routes.get('/providers', ProviderController.index)
+routes.get('/providers/:id/avaliable', AvaliableController.index)
 
 routes.post('/appointments', AppointmentController.store)
 routes.get('/appointments', AppointmentController.index)
